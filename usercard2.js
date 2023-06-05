575942dd19b1a805c095d3a1a5a428c13d5d3048
lokesh199 <41196174+lokesh199@users.noreply.github.com>
2023-06-05 08:30:32 +0530

    added toggle to user card 2

--- usercard2.js
+++ usercard2.js
@@ -1,1 +1,24 @@
-console.log("I am from usercard2")
+let dhoniAsTT = {
+    image: "dhoni_ticket_collector.jpg",
+    title: "MS Dhoni",
+    designation: "Ticket Collector"
+}
+let dhoniAsIndianCaptian = {
+    image: "dhoni_winning_wordcup.jpg",
+    title: "MSD",
+    designation: "Captain of Indian Cricket Team"
+}
+let isDhoniTT = true;
+let currentImg;
+let currentTitle;
+let designation;
+let flipState = function() {
+    if(isDhoniTT){
+        currentImg = dhoniAsIndianCaptian.image;
+        currentTitle = dhoniAsIndianCaptian.title;
+        designation = dhoniAsIndianCaptian.designation;
+        document.getElementById("dhoni-image").src = currentImg;
+        document.getElementById("title").innerHTML = currentTitle;
+        document.getElementById("description").innerHTML = designation;
+    }
+}
